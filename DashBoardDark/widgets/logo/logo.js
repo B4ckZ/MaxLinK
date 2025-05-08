3be1a0f0eeaa53cf4446a9d09a2f8d9dc04204ab
B4ckZ <backzedition@gmail.com>
2025-05-08 20:07:51 +0200

    up

---
+++ DashBoardDark/widgets/logo/logo.js
@@ -0,0 +1,14 @@
+const logoWidget = (function() {
+    function init(element) {
+        console.log('Widget Logo initialisé');
+    }
+    
+    function onResize() {
+        // Fonction vide - requise par l'API des widgets
+    }
+    
+    return {
+        init,
+        onResize
+    };
+})();
